ff0523d33f48f08cf2370278d584ba6bc82eb4c9
Imthiyas786786 <shaik.mohamed.s92@kalvium.community>
2026-03-12 08:40:40 +0530

    1st api_deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is working successfully on Render!"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
